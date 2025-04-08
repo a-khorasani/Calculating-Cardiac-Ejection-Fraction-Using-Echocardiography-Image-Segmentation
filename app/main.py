7de4e8e090fee6a1cf2a04b059442b7ea706aa87
AmirHossein Mahjoub <hossein.astro@yahoo.com>
2025-04-08 14:40:59 +0330

    add fastapi

---
+++ app/main.py
@@ -0,0 +1,78 @@
+from fastapi import FastAPI, File, UploadFile, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from typing import List
+import shutil
+import os
+import torch
+import torchvision.transforms as T
+import numpy as np
+import nibabel as nib
+import segmentation_models_pytorch as smp
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="app/templates")
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# model = torch.load('C:/Users/Amir/Desktop/IPRIA-hackathon-AmirHossein-Mahjoub-Khorasani/unet.pt', 
+#                    map_location=device)
+# model.eval()
+
+transform = T.Compose([
+    T.ToPILImage(),
+    T.Resize((224, 224)),
+    T.ToTensor(),
+])
+
+model = smp.Unet(encoder_name='efficientnet-b1', encoder_weights='imagenet', 
+                in_channels=1, classes=4).to(device)
+model.load_state_dict(torch.load("C:/Users/Amir/Desktop/IPRIA-hackathon-AmirHossein-Mahjoub-Khorasani/unet.pt"))
+model.eval()
+
+def calculate_EF(ed_vol_2CH, es_vol_2CH, ed_vol_4CH, es_vol_4CH):
+    
+    EDV = (ed_vol_2CH + ed_vol_4CH) / 2
+    ESV = (es_vol_2CH + es_vol_4CH) / 2
+
+    EF = ((EDV - ESV) / EDV) * 100
+    return EF
+
+def calculate_volume(img_path):
+    data = nib.load(img_path).get_fdata()
+    data_t = transform(data)
+    data_t = data_t.unsqueeze(0)
+
+    with torch.no_grad():
+        mask = model(data_t)
+        
+    mask = torch.nn.functional.interpolate(mask, 
+                                           size=(data.shape[0], data.shape[1]), 
+                                           mode='bilinear', 
+                                           align_corners=False)
+    mask = np.array(mask)
+    mask = (mask[0, 1, :, :] > 0).astype(np.uint8)  
+    pixel_to_mm = 0.308
+    x_coords, y_coords = np.where(mask == 1)
+    x_min, y_min = np.min(x_coords), y_coords[np.argmin(x_coords)]
+    x_max, y_max = np.max(x_coords), y_coords[np.argmax(x_coords)]
+    L = np.sqrt((x_max - x_min) ** 2 + (y_max - y_min) ** 2) * pixel_to_mm
+    A_l = np.sum(mask) * (pixel_to_mm ** 2) 
+    V = (8 * (A_l ** 2)) / (3 * np.pi * L)
+
+    return V
+
+@app.get("/", response_class=HTMLResponse)
+def main(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/uploadfile/")
+async def upload_file(files: List[UploadFile] = File(...)):
+    volume = []
+    for file in files:
+        volume.append(calculate_volume(file.filename))
+        
+    ef = calculate_EF(volume[0], volume[1], volume[2], volume[3])
+    
+    return {'message': f'ef = {ef:.2f}%'}
